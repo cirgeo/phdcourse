2494bd34d1009fa21de48c6f886559c74030ba60
Pirotti <fpirotti@gmail.com>
2023-03-15 17:38:23 +0100

    updated lecture 2

---
+++ lecture2.R
@@ -0,0 +1,46 @@
+library(terra)
+
+myVariables <- terra::rast("data/variables.tif")
+plot(  myVariables[[1]]  )
+
+originalNames <- names(myVariables)
+names(myVariables) <- c("A", "B", "C", "D")
+names(myVariables)
+names(myVariables) <- originalNames
+
+ndvi <- (myVariables$ndviX1000/1000)-1
+meanTemp <- myVariables$MeanTempX100/100
+stddevTemp <- myVariables$StdDevTempX100/100
+
+myVarRescale <- c( myVariables$Elevation ,
+                             ndvi, meanTemp, stddevTemp)
+
+# terra::crs(myVarRescale)
+# myFirstRaster.3035 <- terra::project(myVariables$Elevation, "epsg:3035")
+# plot(myFirstRaster.3035)
+# png("varPlot.png", width = 2600, height=1800, res=400)
+#  plot(myVariables)
+# dev.off()
+
+# pdf("ndviHist.pdf"  )
+#  terra::hist(myVarRescale$ndviX1000)
+#  terra::hist(myVarRescale$MeanTempX100)
+# dev.off()
+
+vector.raster.ndvi <- terra::values(myVarRescale$ndviX1000)
+vector.raster.elevation <- myVarRescale$Elevation[]
+
+
+
+vector.raster.matrix <- myVarRescale[]
+vector.raster.table <- as.data.frame(myVarRescale[])
+
+
+correlation.table <- cor(vector.raster.matrix)
+
+lm.fit <- lm(vector.raster.table$MeanTempX100 ~ vector.raster.table$Elevation)
+png("correlationPlot.png", width = 2000, height=2000, res=300)
+  smoothScatter( vector.raster.table$Elevation, vector.raster.table$MeanTempX100)
+  abline(lm.fit, lwd=2, col="red")
+  
+dev.off()
