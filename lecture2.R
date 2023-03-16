a5c2d8642ee216b9cda457f1fa85c6ec29a106a8
Pirotti <fpirotti@gmail.com>
2023-03-16 11:12:49 +0100

    autocarty

--- lecture2.R
+++ lecture2.R
@@ -1,7 +1,8 @@
 library(terra)
 
 myVariables <- terra::rast("data/variables.tif")
-plot(  myVariables[[1]]  )
+plot(  myVariables$Elevation  )
+names(myVariables)
 
 originalNames <- names(myVariables)
 names(myVariables) <- c("A", "B", "C", "D")
@@ -12,7 +13,7 @@ ndvi <- (myVariables$ndviX1000/1000)-1
 meanTemp <- myVariables$MeanTempX100/100
 stddevTemp <- myVariables$StdDevTempX100/100
 
-myVarRescale <- c( myVariables$Elevation ,
+myVarRescale <- c( myVariables$Elevation+0 ,
                              ndvi, meanTemp, stddevTemp)
 
 # terra::crs(myVarRescale)
@@ -36,9 +37,11 @@ vector.raster.matrix <- myVarRescale[]
 vector.raster.table <- as.data.frame(myVarRescale[])
 
 
-correlation.table <- cor(vector.raster.matrix)
+correlation.table <- cor(vector.raster.table)
 
 lm.fit <- lm(vector.raster.table$MeanTempX100 ~ vector.raster.table$Elevation)
+summary(lm.fit)
+
 png("correlationPlot.png", width = 2000, height=2000, res=300)
   smoothScatter( vector.raster.table$Elevation, vector.raster.table$MeanTempX100)
   abline(lm.fit, lwd=2, col="red")
@@ -46,4 +49,17 @@ dev.off()
 
 smoothScatter( vector.raster.table$Elevation, vector.raster.table$MeanTempX100)
 abline(lm.fit, lwd=2, col="red")
+
+
+#### sampling -----
+
+sampled.Grid <- terra::spatSample(myVarRescale, size=2000, method="regular", cells=T)
+
+sampled.Grid$LONGITUDE <- terra::xFromCell(myVarRescale, sampled.Grid$cell)
+sampled.Grid$LATITUDE  <- terra::yFromCell(myVarRescale, sampled.Grid$cell)
+
+response <- as.matrix(sampled.Grid$ndviX1000)
+predictors <- sampled.Grid
+predictors$ndviX1000 <- NULL
+locations <- as.matrix(cbind(snow$LONGITUDE, snow$LATITUDE))
 
